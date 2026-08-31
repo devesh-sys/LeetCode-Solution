1513d40e6dd1538493183f18006fde396871ca26
devesh-sys <ss3749617@gmail.com>
2026-08-31 20:45:54 +0530

    Time: 4 ms (85.45%) | Memory: 46.8 MB (60.05%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -1,6 +1,6 @@
 class Solution {
     public String reverseWords(String s) {
-      String[] words=s.split(" ");
+      String[] words=s.split(" ");//important week
       StringBuilder result=new StringBuilder();
       for(int i=0;i<words.length;i++){
         StringBuilder sb=new StringBuilder(words[i]);
