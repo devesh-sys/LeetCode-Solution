95f43e86620815eca1e578918fd65982c675da7a
devesh-sys <ss3749617@gmail.com>
2026-09-13 14:02:15 +0530

    Time: 2 ms (59.67%) | Memory: 43 MB (53.43%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+       Stack<Character> stack1=new Stack<>();
+       Stack<Character> stack2=new Stack<>();
+       for(char ch:s.toCharArray()){
+        if(ch!='#'){
+            stack1.push(ch);
+        }else if(!stack1.isEmpty()){
+            stack1.pop();
+        }
+       }
+       for(char ch:t.toCharArray()){
+        if(ch!='#'){
+            stack2.push(ch);
+        }else if(!stack2.isEmpty()){
+            stack2.pop();
+        }
+       }
+       return stack1.equals(stack2);
+    }
+}
